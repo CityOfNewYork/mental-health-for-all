8f8ac0e0ed4dc785d57c96073fc406de81cf88ea
devowhippit <devonhirth@gmail.com>
2021-07-12 15:27:50 -0400

    Build services json from services

--- bin/services.js
+++ bin/services.js
@@ -8,13 +8,25 @@ const fs = require('fs');
 const cnsl = require('@nycopportunity/pttrn/bin/util/console');
 const alerts = require('@nycopportunity/pttrn/config/alerts');
 
-const services = require('../dist/data/services.json');
+const services = require('../config/slm').services;
 
 const createSlug = (s) => s
   .toLowerCase()
   .replace(/[^0-9a-zA-Z - _]+/g, '')
   .replace(/\s+/g, '-')
   .replace(/-+/g, '-')
+
+const card = {
+  "subtitle": '',
+  "title": '',
+  "programProvider": '',
+  "body": '',
+  "link": '',
+  "featured": '',
+  "category": {},
+  "population": {}
+}
+
 
 /**
  * Export our methods
@@ -23,16 +35,23 @@ const createSlug = (s) => s
  */
 module.exports = {
   run: async () => {
+    let json = [];
+    let servciesJson = 'dist/data/services.json';
+
     for (let i = services.length - 1; i >= 0; i--) {
       let service = services[i];
-      let template = 'src/slm/services/main.slm';
+
+      /**
+       * Create service view
+       */
+      let template = 'src/slm/services/service.slm';
       let data = fs.readFileSync(template, 'utf8');
+      let slug = createSlug(service.title);
+      let write = `src/views/programs/${slug}.slm`;
 
       data = data
         .replace(/{{ SERVICE_TITLE }}/g, service.title)
-        .replace('{{ SERVICE_SLUG }}', createSlug(service.title));
-
-      let write = `src/views/programs/${createSlug(service.title)}.slm`;
+        .replace('{{ SERVICE_SLUG }}', slug);
 
       // if (!fs.existsSync(write)) {
         await fs.writeFileSync(write, data);
@@ -41,6 +60,30 @@ module.exports = {
       // } else {
         // cnsl.error(`${alerts.str.path(write)} already exists.`);
       // }
+
+      /**
+       * Build card data for services.json
+       */
+
+      let srvc = {};
+
+      Object.keys(card).map(key => {
+        if (key === 'body') {
+          srvc[key] = service['subtitle'];
+        } else {
+          srvc[key] = service[key];
+        }
+      });
+
+      json.push(srvc);
     }
+
+    /**
+     * Write the services json
+     */
+
+    fs.writeFileSync(servciesJson, JSON.stringify(json));
+
+    cnsl.success(`${alerts.str.path(servciesJson)} was made.`);
   }
 };
