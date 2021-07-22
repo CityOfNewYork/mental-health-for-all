575fe805fdcc8ae5451a85bcdb5b4057a933012a
Yonas Fesehatsion <yone920@gmail.com>
2021-07-22 16:56:16 -0400

    About Us page added

--- config/slm.js
+++ config/slm.js
@@ -2,6 +2,7 @@ let package = require('../package.json');
 let services = require('./services');
 let banners = require('./banners');
 let substanceAbuse = require('./substance-abuse');
+let aboutUs = require('./about-us');
 let population = require('./population.json');
 
 let remotes = {
@@ -43,6 +44,7 @@ module.exports = {
     'Substance Use Services',
   ],
   population: population,
+  aboutUs: aboutUs,
   generateClassName: (title) => {
     let className = `bg-${title.toLowerCase()}--secondary`;
     return className;
