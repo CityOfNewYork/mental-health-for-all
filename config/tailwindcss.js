ee4f57e98ebdd9a86c5bf0c3b239815255d8ae44
devowhippit <devonhirth@gmail.com>
2021-07-28 11:52:45 -0400

    Add updated screen sizes to tailwind theme

--- config/tailwindcss.js
+++ config/tailwindcss.js
@@ -5,6 +5,15 @@ tailwindcss.theme.colors = tokens.colors;
 tailwindcss.theme.textColor = tokens.colors;
 tailwindcss.theme.backgroundColor = tokens.colors;
 tailwindcss.theme.borderColor = tokens.colors;
-// tailwindcss.theme.fontFamily = tokens.fonts;
+
+tailwindcss.theme.screens = {
+  'mobile-only': {
+    'max': `${parseInt(tokens.screens['small']) - parseFloat(tokens.screens['max-width-offset'])}px` // Max widths should always use the offset
+  },
+  'mobile':      { 'min': tokens.screens['small']  },
+  'tablet':      { 'min': tokens.screens['medium'] },
+  'desktop':     { 'min': tokens.screens['large']  },
+  'desktop-lg':  { 'min': tokens.screens['xlarge'] }
+};
 
 module.exports = tailwindcss;
