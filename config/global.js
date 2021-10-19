6c9a328262e8b029bc99e1e8a6cf6e60aeebf0da
devowhippit <devonhirth@gmail.com>
2021-10-19 13:41:41 -0400

    Enable build directories for downstream envs

--- config/global.js
+++ config/global.js
@@ -1,8 +1,8 @@
 let global = require('@nycopportunity/pttrn/config/global');
 
 // This will override the global dist folder based on the Node ENV variable
-// if (process.env.NODE_ENV !== 'development' && process.env.NODE_ENV !== 'testing') {
-//   global.dist = process.env.NODE_ENV;
-// }
+if (process.env.NODE_ENV !== 'development' && process.env.NODE_ENV !== 'testing') {
+  global.dist = process.env.NODE_ENV;
+}
 
 module.exports = global;
